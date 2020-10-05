47df0a9c30f00e51d2294d1393dbfab90ce0bfc4
Leif Battermann <leif.battermann@gmail.com>
2020-10-05 21:34:24 +0200

    post-flop

--- Player.js
+++ Player.js
@@ -18,11 +18,25 @@ class Player {
       } else {
         // @todo: throw if pot does not return a number?
         bet(game.pot());
-    }} else {
-      bet(game.pot());
+    }} 
+  
+    // post flop
+    else {
+      var communityCards = game.communityCards(); // open cards in middle
+      var holeCards = game.me().holeCards(); // our initial cards
+
+      var haveAtLeastPair = communityCards.map(card => card.rank()).includes(holeCards[0].rank()) || 
+      communityCards.map(card => card.rank()).includes(holeCards[1].rank())
+      
+      if(haveAtLeastPair) {
+        bet(game.pot());
+      }
+      else {
+        bet(fold);
+      }
     }
 
-     // post flop
+     
   }
 
   static showdown(gameState) {
